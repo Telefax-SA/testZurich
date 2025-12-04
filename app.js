b81374ee9e68ca54edf2daf5eb790cc9318c1c69
NahuelM <nahuelm210@gmail.com>
2025-12-04 12:55:26 -0300

    test2

--- app.js
+++ app.js
@@ -1,37 +1,4 @@
-async function enviarEmail() {
-
-  const destinatario = document.getElementById("dest").value;
-  const asunto = document.getElementById("asunto").value;
-  const mensaje = document.getElementById("mensaje").value;
-
-  const url = "http://192.168.100.5:44370/Notificador.asmx";
-
-  const soapEnvelope =
-    `<?xml version="1.0" encoding="utf-8"?>
-    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-      <soap:Body>
-        <Email xmlns="http://tempuri.org/">
-          <destinatario>${destinatario}</destinatario>
-          <asunto>${asunto}</asunto>
-          <mensaje>${mensaje}</mensaje>
-        </Email>
-      </soap:Body>
-    </soap:Envelope>`;
-
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "text/xml; charset=utf-8",
-        "SOAPAction": "http://tempuri.org/Email"
-      },
-      body: soapEnvelope
-    });
-
-    const text = await response.text();
-    document.getElementById("respuesta").textContent = text;
-
-  } catch (error) {
-    document.getElementById("respuesta").textContent = "Error: " + error;
-  }
-}
+fetch("https://localhost:3000/ping")
+  .then(r => r.json())
+  .then(console.log)
+  .catch(err => console.error("Error:", err));
